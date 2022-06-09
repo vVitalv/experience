015d9fde4785b06c670c8aefa36f1e85ae1f882d
vVitalv <vitalvital@bk.ru>
2022-06-09 23:54:07 +0300

    observer

--- client/components/content.js
+++ client/components/content.js
@@ -1,15 +1,36 @@
-import React from "react"
+import React, { useRef, useEffect, useState } from "react"
 
 import ReactLogo from "./reactlogo"
 import Hi from "./section1"
 import H1 from "./h1"
 
 const Content = () => {
+  const s1 = useRef()
+  const [s1Width, setS1Width] = useState(0)
+  const observerCallback = (entries) => {
+    if (entries[0].isIntersecting) {
+      setS1Width(Math.round(entries[0].intersectionRatio * 100))
+      console.log(s1Width)
+    }
+  }
+  const observerOptions = {
+    rootMargin: "50% 0px 0px 0px",
+    threshold: [0, 0.2, 0.4, 0.6, 0.8, 1],
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(observerCallback, observerOptions)
+    if (s1.current) observer.observe(s1.current)
+    return () => {
+      if (s1.current) observer.unobserve(s1.current)
+    }
+  }, [s1, observerOptions])
+
   return (
     <main className='flex flex-col items-center w-full min-h-fit px-2 bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900'>
       <ReactLogo />
-      <div className='flex flex-col items-center w-full'>
-        <H1 hText={`Greetings ${"\uD83D\uDC4B"}`} />
+      <div ref={s1} className='flex flex-col items-center w-full'>
+        <H1 hText={`Greetings ${"\uD83D\uDC4B"}`} width={s1Width} />
         <Hi />
       </div>
       <div className='flex flex-col items-center w-full'>
--- client/components/h1.js
+++ client/components/h1.js
@@ -1,9 +1,14 @@
 import React from "react"
 
-const H1 = ({ hText }) => {
+const H1 = ({ hText, width }) => {
+  const style = { width: `${width}%` }
   return (
-    <div className='sticky top-16 bg-purple-900 bg-opacity-20 p-4 w-full rounded-full'>
-      <h1>{hText}</h1>
+    <div className='sticky top-16 w-full rounded-full border border-purple-400'>
+      <div
+        className='absolute h-full bg-purple-600 transition duration-300 rounded-full bg-opacity-20'
+        style={style}
+      />
+      <h1 className='p-4'>{hText}</h1>
     </div>
   )
 }
