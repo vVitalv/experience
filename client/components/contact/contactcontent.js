bdd7eea9c9b2bcfb9e1428a0f27f3a4d22240bfe
vVitalv <vitalvital@bk.ru>
2022-06-16 23:14:07 +0300

    contacts

--- client/components/contact/contactcontent.js
+++ client/components/contact/contactcontent.js
@@ -5,32 +5,87 @@ import PopUpMsg from "../popupmsg"
 
 const ContactContent = () => {
   const [popup, togglePopup] = useState(false)
+  const [msgStatus, setStatus] = useState("ok")
+  const msgs = {
+    email: {
+      ok: "Email address copied to clipboard",
+      err: "Copy error",
+    },
+    telegram: {
+      ok: "Telegram account name copied to clipboard",
+      err: "Copy error",
+    },
+  }
+  function showPopup(e, type) {
+    navigator.clipboard
+      .writeText(e.target.innerText)
+      .then(() => {
+        togglePopup(type)
+        setStatus("ok")
+      })
+      .catch(() => {
+        togglePopup(type)
+        setStatus("err")
+      })
+  }
+
   return (
-    <main className='flex flex-col w-full h-full px-4 py-24 text-gray-200 bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900'>
-      <div>
-        E-mail{" "}
-        <button
-          type='button'
-          onClick={(e) => {
-            navigator.clipboard
-              .writeText(e.target.innerText)
-              .then(() => togglePopup(true))
-          }}
-        >
-          vitalvital@bk.ru
-        </button>
+    <main className='flex justify-center items-center w-full h-full text-gray-200 bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900'>
+      <div className='flex flex-col gap-4 w-72 sm:w-80 px-4 py-24'>
+        <div className='flex w-full justify-between'>
+          <p>E-mail </p>
+          <button
+            className='text-gray-400 hover:text-purple-400 transition duration-300'
+            type='button'
+            onClick={(e) => showPopup(e, "email")}
+          >
+            vitalvital@bk.ru {"\uD83D\uDCCB"}
+          </button>
+        </div>
+        <div className='flex w-full justify-between'>
+          <p>Telegram </p>
+          <button
+            className='text-gray-400 hover:text-purple-400 transition duration-300'
+            type='button'
+            onClick={(e) => showPopup(e, "telegram")}
+          >
+            @dustonthedust {"\uD83D\uDCCB"}
+          </button>
+        </div>
+        <div className='flex w-full justify-between'>
+          <p>GitHub </p>
+          <button
+            className='text-gray-400 hover:text-purple-400 transition duration-300'
+            type='button'
+            onClick={() => window.open("https://github.com/vVitalv/", "_blank")}
+          >
+            github.com {"\uD83C\uDF0D"}
+          </button>
+        </div>
+        <div className='flex w-full justify-between'>
+          <p>LinkedIn </p>
+          <button
+            className='text-gray-400 hover:text-purple-400 transition duration-300'
+            type='button'
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/vitaly-volkov-988aa81b7/",
+                "_blank"
+              )
+            }
+          >
+            linkedin.com {"\uD83C\uDF0D"}
+          </button>
+        </div>
         {popup &&
           createPortal(
             <PopUpMsg
-              msg='email copied to clipboard'
-              err={false}
+              msg={msgStatus === "ok" ? msgs[popup].ok : msgs[popup].err}
+              err={msgStatus}
               remove={togglePopup}
             />,
             document.body
           )}
-      </div>
-      <div>
-        Telegram <span>@dustonthedust</span>
       </div>
     </main>
   )
--- client/components/popupmsg.js
+++ client/components/popupmsg.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-const PopUpMsg = ({ msg, err, remove }) => {
-  const sign = err ? "\u2718" : "\u2714"
-  const textColor = err ? "text-red-600" : "text-amber-500"
+const PopUpMsg = ({ msg, status, remove }) => {
+  const sign = status === 'err' ? "\u2718" : "\u2714"
+  const textColor = status === "err" ? "text-red-300" : "text-green-300"
   return (
     <div
-      className={`fixed animate-[popup_4s] right-2 max-w-48 h-fit p-4 rounded-md text-sm bg-opacity-50 bg-gray-400 ${textColor}`}
+      className={`fixed animate-[popup_4s] right-0 max-w-sm h-fit m-2 p-4 rounded-md text-sm bg-opacity-50 bg-gray-400 ${textColor}`}
       onAnimationEnd={() => remove(false)}
     >
       {sign} {msg}
